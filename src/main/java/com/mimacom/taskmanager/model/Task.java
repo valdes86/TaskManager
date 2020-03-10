69e961aba8bdb5369e20cf068c047eb22f8ded9d
Pablo Rodríguez Valdés <valdes86@gmail.com>
2020-03-10 20:50:02 +0100

    Swagger configured, API documented

--- src/main/java/com/mimacom/taskmanager/model/Task.java
+++ src/main/java/com/mimacom/taskmanager/model/Task.java
@@ -5,25 +5,36 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Entity
 @Data
 @Table(name = "TASK")
+@ApiModel("Task entity")
 public class Task {
 
 	@Id
 	@GeneratedValue
 	@Column(name = "ID_TASK")
+	@ApiModelProperty("Task id. value")
+	@NotNull
 	private Long idTask;
 	@Column(name = "TITLE")
+	@ApiModelProperty("Title of the task")
+	@NotNull
 	private String title;
+	@ApiModelProperty("Description of the task")
 	@Column(name = "DESCRIPTION")
 	private String description;
-	@Column(name = "DONE")
+	@Column(name = "DONE", columnDefinition = "boolean default false")
+	@ApiModelProperty("Flag for DONE property, default: false")
 	private Boolean done;
-	@Column(name = "FINISHED")
+	@Column(name = "FINISHED", columnDefinition = "boolean default false")
+	@ApiModelProperty("Flag for FINISHED property, default: false")
 	private Boolean finished;
 
 	public Task() {
